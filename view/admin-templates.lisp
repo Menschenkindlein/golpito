84821a5f391ccc13b3ecfbd5868406b720b7e89f
Zholobak Maxim <ghostcheka@gmail.com>
2014-03-22 15:08:52 +0200

    engine: improving admin templates

--- view/admin-templates.lisp
+++ view/admin-templates.lisp
@@ -5,53 +5,22 @@
    (make-environment (list :article article))))
 
 (defun tmpl-edit-author (author)
-  (with-slots
-        (name title description)
-      author
-    (format nil
-            "<html>
-  <head>
-    <title>Edit author</title>
-  </head>
-  <body>
-    <h1>Edit author</h1>
-    <form action=\"~a\" method=\"post\">
-      <input type=\"hidden\" value=\"~a\" name=\"name\" />
-      <label><input type=\"text\" value=\"~a\" name=\"title\" /> Имя автора</label><br />
-      <label><textarea name=\"description\">~a</textarea> Краткое описание особенностей автора</label><br />
-      <input type=\"submit\" />
-    </form>
-  </body>
-</html>"
-            (restas:genurl *edit-author-page-symbol*)
-            name
-            title
-            description)))
+  (golpito.view.tmpl:edit-category-tmpl
+   (make-environment (list :category author
+                           :name "author"
+                           :name-label "Имя автора"
+                           :name-description "Краткое описание особенностей автора"))))
 
 (defun tmpl-edit-category (category)
   (golpito.view.tmpl:edit-category-tmpl
-   (make-environment (list :category category))))
+   (make-environment (list :category category
+                           :name "category"
+                           :name-label "Название категории"
+                           :name-description "Краткое описание категории"))))
 
 (defun tmpl-edit-tag (tag)
-  (with-slots
-        (name title description)
-      tag
-    (format nil
-            "<html>
-  <head>
-    <title>Edit tag</title>
-  </head>
-  <body>
-    <h1>Edit tag</h1>
-    <form action=\"~a\" method=\"post\">
-      <input type=\"hidden\" value=\"~a\" name=\"name\" />
-      <label><input type=\"text\" value=\"~a\" name=\"title\" /> Полное имя тега</label><br />
-      <label><textarea name=\"description\">~a</textarea> Краткое описание тега</label><br />
-      <input type=\"submit\" />
-    </form>
-  </body>
-</html>"
-            (restas:genurl *edit-tag-page-symbol*)
-            name
-            title
-            description)))
+  (golpito.view.tmpl:edit-category-tmpl
+   (make-environment (list :category tag
+                           :name "tag"
+                           :name-label "Полное имя тега"
+                           :name-description "Краткое описание тега"))))
